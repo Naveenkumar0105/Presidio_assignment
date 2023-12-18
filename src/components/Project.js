760aab1e63a8dcfc273d85f9b548d082fe0c4b2a
Naveenkumar <107810903+Naveenkumar0105@users.noreply.github.com>
2023-12-18 10:28:47 +0530

    Add files via upload

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react';
-// import image from '../img/image.png';
-import { HeartIcon, EyeIcon } from "@heroicons/react/20/solid";
+import { HeartIcon, EyeIcon, BookmarkIcon } from "@heroicons/react/20/solid";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
 
-function Project({image}) {
+function Project({ image, name, c_name }) {
   const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
   const [count, setCount] = useState(0);
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
   const handleLike = () => {
     setLiked(!liked);
     setCount((prevCount) => (liked ? prevCount - 1 : prevCount + 1));
   };
 
+  const handleBookmark = () => {
+    setBookmarked(!bookmarked);
+  };
+
   return (
     <div className='w-[340px] h-[270px] mb-20'>
-      <img className='rounded-xl w-[350px] h-[250px] px-2' src={image} alt='creative shots'/>
+      <div
+        className={`w-[350px] h-[250px] px-2 rounded-xl relative group overflow-hidden`}
+        onMouseEnter={() => setIsOverlayVisible(true)}
+        onMouseLeave={() => setIsOverlayVisible(false)}
+      >
+        <img className='absolute top-0 left-0 w-full h-full object-cover' src={image} alt='creative shots' />
+        <div
+          className={`absolute bottom-0 left-0 w-full h-16 px-4 flex items-center bg-black/70 opacity-0 justify-between z-10 transition-opacity duration-150 ease-in-out ${
+            isOverlayVisible ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <p className='text-white font-semibold'>{name}</p>
+          <div className="flex space-x-2">
+            <HeartIcon className={`text-white h-5 w-5 cursor-pointer ${liked ? 'text-pink-500' : 'text-gray-400'} hover:text-pink-500`} onClick={handleLike} />
+            <BookmarkIcon className={`text-white h-5 w-5 cursor-pointer ${bookmarked ? 'text-white' : 'text-gray-400'}`} onClick={handleBookmark} />
+          </div>
+        </div>
+      </div>
       <div className='flex justify-between p-2'>
         <div className='flex items-center'>
           <UserCircleIcon className="h-8 text-gray-400" />
-          <h3 className='font-semibold text-black text-[15px]'>Dmitriy Golovenko</h3>
+          <h3 className='font-semibold text-black text-[15px]'>{c_name}</h3>
           <h3 className='bg-gray-300 text-white text-xs font-bold rounded-md ml-2 px-1'>TEAM</h3>
         </div>
         <div className='flex space-x-2 '>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,42 +21,38 @@ import image18 from "../img/image18.jpeg";
 import image19 from "../img/image19.jpeg";
 import image20 from "../img/image20.jpeg";
 
-// function Projects() {
-//   return (
-//     <div className='flex flex-wrap  justify-evenly mt-10'>
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//         <Project />
-//     </div>
-//   )
-// }
-
 function Projects() {
     const images = [
-        image,
-        image2,
-        image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,
-        image13,image14,image15,image16,image17,image18,image19,image20
-        
+      { creator_name:"creator 1" ,name: "image 1", image: image },
+      { creator_name:"creator 2" ,name: "image 2", image: image2 },
+      { creator_name:"creator 3" ,name: "image 3", image: image3 },
+      { creator_name:"creator 4" ,name: "image 4", image: image4 },
+      { creator_name:"creator 5" ,name: "image 5", image: image5 },
+      { creator_name:"creator 6" ,name: "image 6", image: image6 },
+      { creator_name:"creator 7" ,name: "image 7", image: image7 },
+      { creator_name:"creator 8" ,name: "image 8", image: image8 },
+      { creator_name:"creator 9" ,name: "image 9", image: image9 },
+      { creator_name:"creator 10" ,name: "image 10", image: image10 },
+      { creator_name:"creator 11" ,name: "image 11", image: image11 },
+      { creator_name:"creator 12" ,name: "image 12", image: image12 },
+      { creator_name:"creator 13" ,name: "image 13", image: image13 },
+      { creator_name:"creator 14" ,name: "image 14", image: image14 },
+      { creator_name:"creator 15" ,name: "image 15", image: image15 },
+      { creator_name:"creator 16" ,name: "image 16", image: image16 },
+      { creator_name:"creator 17" ,name: "image 17", image: image17 },
+      { creator_name:"creator 18" ,name: "image 18", image: image18 },
+      { creator_name:"creator 19" ,name: "image 19", image: image19 },
+      { creator_name:"creator 20" ,name: "image 20", image: image20 }
     ];
-
-    return (
-        <div className="flex flex-wrap justify-evenly mt-10">
-        {images.map((image) => (
-            <Project key={image} image={image} />
-        ))}
-        </div>
-    );
-}
   
-
-export default Projects
+    return (
+      <div className="flex flex-wrap justify-evenly mt-10">
+        {images.map((image) => (
+          <Project key={image.name} image={image.image} name={image.name} c_name={image.creator_name} />
+        ))}
+      </div>
+    );
+  }
+  
+  export default Projects;
+  
